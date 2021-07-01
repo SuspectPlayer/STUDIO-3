ef0c5ebde0f8505167a89a96a0446c347d5552ba
Jack <1018019@student.sae.edu.au>
2021-07-01 11:53:24 +1000

    Added and modified controllers to be able to chat with one another

---
+++ VR_Project_Studio3/Assets/Scripts/MultiplerComonents/PlayerComonents/VoiceAdjustments.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Voice;
+using Photon.Voice.PUN;
+using Photon.Voice.Unity;
+
+public class VoiceAdjustments : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
