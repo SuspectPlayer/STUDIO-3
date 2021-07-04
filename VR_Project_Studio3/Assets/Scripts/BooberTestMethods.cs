0acadc8381859c44182f6c89e0b4a98a82a4be39
HosekiAnankos <jaspervonriegen@gmail.com>
2021-07-04 19:33:48 +1000

    Creating Hand Scanner Interactor

---
+++ VR_Project_Studio3/Assets/Scripts/BooberTestMethods.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BooberTestMethods : MonoBehaviour
+{
+    public void TouchTest()
+    {
+        Debug.Log("Booped");
+    }
+
+    public void ReleaseTest()
+    {
+        Debug.Log("Unbooped");
+    }
+
+    public void HoldTest()
+    {
+        Debug.Log("Snooper-dooped");
+    }
+}
