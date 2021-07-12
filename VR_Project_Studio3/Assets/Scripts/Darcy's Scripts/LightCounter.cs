ec6720c06e1c50514b5170283b7c576fd3a5c8d1
Darcy Glover <darcy.glover414@gmail.com>
2021-07-12 12:38:25 +1000

    changes

--- VR_Project_Studio3/Assets/Scripts/Darcy's Scripts/LightCounter.cs
+++ VR_Project_Studio3/Assets/Scripts/Darcy's Scripts/LightCounter.cs
@@ -9,39 +9,7 @@ public class LightCounter : MonoBehaviour
 {
     public int lightCount = 0;
 
-    [SerializeField]
-    GameObject[] lightsOn, lightsOff;
-
-    void Update()
-    {
-        switch (lightCount) //controls the lights on the map to be turned on and off depending on how many lights are on
-        {
-            case 0:
-                {
-                    lightsOn[0].SetActive(false);
-                    lightsOff[0].SetActive(true);
-                    lightsOn[1].SetActive(false);
-                    lightsOff[1].SetActive(true);
-                    break;
-                }
-            case 1:
-                {
-                    lightsOn[0].SetActive(true);
-                    lightsOff[0].SetActive(false);
-                    lightsOn[1].SetActive(false);
-                    lightsOff[1].SetActive(true);
-                    break;
-                }
-            case 2:
-                {
-                    lightsOn[0].SetActive(true);
-                    lightsOff[0].SetActive(false);
-                    lightsOn[1].SetActive(true);
-                    lightsOff[1].SetActive(false);
-                    break;
-                }
-        }
-    }
+    public GameObject[] lights;
 
     public void CountUp()
     {
